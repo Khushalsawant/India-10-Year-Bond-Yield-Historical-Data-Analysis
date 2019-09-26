27c5d89a6f43b73cea39aac32a42b6a0a9d6992f
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-09-26 09:21:53 +0530

    Add files via upload

--- IND_10Y_BOND_Historical_data.py
+++ IND_10Y_BOND_Historical_data.py
@@ -17,6 +17,12 @@ from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 import sys
 
+from sklearn.metrics import mean_squared_error 
+from numpy import sqrt
+
+#fit the model
+from statsmodels.tsa.vector_ar.var_model import VAR
+
 '''
 if sys.version_info >= (3,):
     import urllib.request as urllib2
@@ -72,7 +78,7 @@ def scrap_data_from_websites(historical_bond_data_url,column_names,last_date_rec
         scrap_data_df = scrap_data_df.sort_index(ascending=True,axis=0)
         #print(scrap_data_df['Date'][len(scrap_data_df['Date'])-1])
         scrap_data_df = scrap_data_df.loc[scrap_data_df['Date'] > last_date_rec]
-        print(scrap_data_df)
+        #print(scrap_data_df)
         
     else:
         print("Error in connecting to ",historical_bond_data_url,"having status code as ",res.status_code)
@@ -90,12 +96,12 @@ def get_latest_Bond_Yield_historical_data():
     
     scrap_data_df = scrap_data_from_websites(historical_bond_data_url,column_names,last_date_rec)
     latest_historical_data = pd.concat([Historical_data,scrap_data_df]).sort_index(ascending=True,axis=0)#.drop_duplicates(subset ="Date", keep = False, inplace = True)
-    print(latest_historical_data)
-    print(len(Historical_data),len(scrap_data_df))
-    return latest_historical_data
+    print(latest_historical_data.head())
+    #print(len(Historical_data),len(scrap_data_df))
+    return latest_historical_data.reset_index(drop=True)
 
 def get_latest_USD_INR_Historical_data():
-    path_of_file = "D:/USD_INR Historical Data.csv"
+    path_of_file = "file:///D:/USD_INR Historical Data.csv"
     Historical_data = read_Historical_data_file(path_of_file)
     last_date_rec = Historical_data['Date'][len(Historical_data)-1]
     #print(last_date_rec)
@@ -104,18 +110,82 @@ def get_latest_USD_INR_Historical_data():
     
     scrap_data_df = scrap_data_from_websites(historical_bond_data_url,column_names,last_date_rec)
     latest_historical_data = pd.concat([Historical_data,scrap_data_df]).sort_index(ascending=True,axis=0)#.drop_duplicates(subset ="Date", keep = False, inplace = True)
-    print(latest_historical_data)
-    print(len(Historical_data),len(scrap_data_df))
-    return latest_historical_data
+    print(latest_historical_data.head())
+    #print(len(Historical_data),len(scrap_data_df))
+    return latest_historical_data.reset_index(drop=True)
 
-latest_Bond_Yield_historical_data = get_latest_Bond_Yield_historical_data()
+Bond_Yield_historical_data = get_latest_Bond_Yield_historical_data()
 
 USD_INR_Historical_Data = get_latest_USD_INR_Historical_data()
 
 # Merging the dataframes                       
-latest_historical_data = pd.merge(left=latest_Bond_Yield_historical_data, right=USD_INR_Historical_Data, right_on ='Date', left_on ='Date') 
+latest_historical_data = pd.merge(left=Bond_Yield_historical_data, right=USD_INR_Historical_Data, right_on ='Date', left_on ='Date') 
 
 n =  time.time() - start_time
 
+#print(latest_historical_data.dtypes)
+
+latest_historical_data['Price_x']  = latest_historical_data['Price_x'].astype(float)
+latest_historical_data['Open_x']  = latest_historical_data['Open_x'].astype(float)
+latest_historical_data['High_x']  = latest_historical_data['High_x'].astype(float)
+latest_historical_data['Low_x']  = latest_historical_data['Low_x'].astype(float)
+
+latest_historical_data['Price_y']  = latest_historical_data['Price_y'].astype(float)
+latest_historical_data['Open_y']  = latest_historical_data['Open_y'].astype(float)
+latest_historical_data['High_y']  = latest_historical_data['High_y'].astype(float)
+latest_historical_data['Low_y']  = latest_historical_data['Low_y'].astype(float)
+
+print(latest_historical_data.head())
+
+#cols = latest_historical_data.columns
+
+#checking stationarity
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+#since the test works for only 12 variables, I have randomly dropped
+#in the next iteration, I would drop another and check the eigenvalues
+johan_test_temp = latest_historical_data.drop(['Date', 'Change %_x','Change %_y'], axis=1)
+coint_johansen(johan_test_temp,-1,1).eig
+
+print(np.asarray(johan_test_temp))
+print(johan_test_temp.dtypes)
+
+cols = johan_test_temp.columns
+
+#creating the train and validation set
+train = johan_test_temp[:int(0.90*(len(johan_test_temp)))]
+valid = johan_test_temp[int(0.90*(len(johan_test_temp))):]
+
+model = VAR(endog=train)
+model_fit = model.fit()
+
+# make prediction on validation
+prediction = model_fit.forecast(model_fit.y, steps=len(valid))
+
+#print(prediction)
+#converting predictions to dataframe
+pred = pd.DataFrame(index=range(0,len(prediction)),columns=[cols])
+
+for j in range(0,8):
+    for i in range(0, len(prediction)):
+       pred.iloc[i][j] = prediction[i][j]
+
+print(pred)
+
+#check rmse
+for i in cols:
+    #rms = np.sqrt(np.mean(np.power((valid[i]-pred[i]),2)))
+    print('rmse value for', i, 'is : ', sqrt(mean_squared_error(pred[i], valid[i])))
+    #print('rmse value for', i, 'is : ',rms)
+
+#make final predictions
+model = VAR(endog=johan_test_temp)
+model_fit = model.fit()
+yhat = model_fit.forecast(model_fit.y, steps=1)
+print(yhat)
+
+'''
+for i in range(len(cols)):
+    print("Predicted value of ", cols[i] ," is : ",yhat[i])    
+'''
 
 print("---Execution Time ---",convert_sec(n))
